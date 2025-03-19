c8e0b938f47b7d20ed142c7ab35ad1279d08c880
anthonyging12 <65613944+anthonyging12@users.noreply.github.com>
2025-03-19 10:47:54 -0400

    updated settings3

--- src/index.js
+++ src/index.js
@@ -29,19 +29,17 @@ app.use('/api/recommendations', recommendationRoutes);
 app.use('/api/watchlist', watchlistRoutes);
 app.use('/api/reviews', reviewRoutes);
 
-const PORT = process.env.PORT || 4000;
-sequelize
-  .sync()
-  .then(() => {
-    console.log('Database connected and synchronized!');
-    console.log(`ENV: ${process.env.NODE_ENV || 'development'}`);
-    console.log(`PORT: ${PORT}`);
-    // Bind to 0.0.0.0 instead of default
-    app.listen(PORT, '0.0.0.0', () => {
-      console.log(`Backend server running on port ${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error('Database connection error:', error);
-  });
 
+  const PORT = process.env.PORT || 4000;
+  sequelize
+    .sync()
+    .then(() => {
+      console.log('Database connected and synchronized!');
+      
+      app.listen(PORT, '0.0.0.0', () => console.log(`Backend server running on port ${PORT}`));
+
+    })
+    .catch((error) => {
+      console.error('Database connection error:', error);
+    });
+  
